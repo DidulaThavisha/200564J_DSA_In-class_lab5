8c5fb3590b1b28ff5a257385581b3d13c07f578a
Didula Samaraweera <samaraweeradt.20@uom.lk>
2023-03-28 17:40:47 +0530

    Update Recursive_Non-recursive.cpp

--- Recursive_Non-recursive.cpp
+++ Recursive_Non-recursive.cpp
@@ -1,65 +1,10 @@
-
 #include <iostream>
 #include <vector>
-#include <stack>
 #include <chrono>
 #include <random>
-using namespace std;
-using namespace std::chrono;
 
 using namespace std;
 
-int partition(vector<int>& arr, int low, int high)
-{
-    int pivot = arr[high];
-    int i = low - 1;
-    for(int j=low; j<=high-1; j++)
-    {
-        if(arr[j] < pivot)
-        {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i+1], arr[high]);
-    return i+1;
-}
-
-void quickSort(vector<int>& arr, int low, int high)
-{
-    if(low < high)
-    {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi-1);
-        quickSort(arr, pi+1, high);
-    }
-}
-
-void quickSortNonRecursive(vector<int>& arr, int low, int high)
-{
-    stack<int> stk;
-    stk.push(low);
-    stk.push(high);
-
-    while(!stk.empty())
-    {
-        int end = stk.top();
-        stk.pop();
-        int start = stk.top();
-        stk.pop();
-
-        if(start < end)
-        {
-            int pi = partition(arr, start, end);
-
-            stk.push(start);
-            stk.push(pi-1);
-
-            stk.push(pi+1);
-            stk.push(end);
-        }
-    }
-}
 vector<int> generateRandomArray(int n) {
     vector<int> arr(n);
 
@@ -75,29 +20,85 @@ vector<int> generateRandomArray(int n) {
 
     return arr;
 }
-int main()
-{
-    for (int k=100; k<=1000;k+=100){
-        vector<int> arr = generateRandomArray(k);
-        int n = arr.size();
-    
-        // Recursive Quick Sort
-        auto start1 = high_resolution_clock::now();
-        quickSort(arr, 0, n-1);
-        auto end1 = high_resolution_clock::now();
-        auto duration1 = duration_cast<microseconds>(end1 - start1);
-    
-        // Non-recursive Quick Sort
-        auto start2 = high_resolution_clock::now();
-        quickSortNonRecursive(arr, 0, n-1);
-        auto end2 = high_resolution_clock::now();
-        auto duration2 = duration_cast<microseconds>(end2 - start2);
-        cout << "Array size"<< k << endl;
-        cout << "Recursive Quick Sort: " << duration1.count() << " microseconds" << endl;
-        cout << "Non-recursive Quick Sort: " << duration2.count() << " microseconds" << endl;
-    
-    
+
+void recursiveQuickSort(vector<int>& arr, int left, int right) {
+    if (left >= right)
+        return;
+
+    int i = left, j = right;
+    int pivot = arr[(left + right) / 2];
+
+    while (i <= j) {
+        while (arr[i] < pivot)
+            i++;
+        while (arr[j] > pivot)
+            j--;
+        if (i <= j) {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
     }
-    return 0;
+
+    recursiveQuickSort(arr, left, j);
+    recursiveQuickSort(arr, i, right);
 }
 
+void iterativeQuickSort(vector<int>& arr, int left, int right) {
+    vector<pair<int, int>> stk;
+    stk.push_back({left, right});
+
+    while (!stk.empty()) {
+        int start = stk.back().first, end = stk.back().second;
+        stk.pop_back();
+
+        if (start < end) {
+            int i = start, j = end;
+            int pivot = arr[(start + end) / 2];
+
+            while (i <= j) {
+                while (arr[i] < pivot)
+                    i++;
+                while (arr[j] > pivot)
+                    j--;
+                if (i <= j) {
+                    swap(arr[i], arr[j]);
+                    i++;
+                    j--;
+                }
+            }
+
+            stk.push_back({start, j});
+            stk.push_back({i, end});
+        }
+    }
+}
+
+int main() {
+    for (int i=100; i<1000; i+=100){
+        int n=i;
+        vector<int> arr = generateRandomArray(n);
+        
+        //array size
+        cout << "Array size: " << n << endl;
+        
+        // Recursive Quick Sort
+        auto start = chrono::high_resolution_clock::now();
+        recursiveQuickSort(arr, 0, n - 1);
+        auto end = chrono::high_resolution_clock::now();
+        auto recursiveDuration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    
+        // Iterative Quick Sort
+        start = chrono::high_resolution_clock::now();
+        iterativeQuickSort(arr, 0, n - 1);
+        end = chrono::high_resolution_clock::now();
+        auto iterativeDuration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    
+        cout << "Iterative Quick Sort time: " << recursiveDuration.count() << " nanoseconds" << endl;
+        cout << "Recursive Quick Sort time: " << iterativeDuration.count() << " nanoseconds" << endl;
+    }
+    
+    
+
+    return 0;
+}
